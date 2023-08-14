b02deb5d233c32e921786b9e645617deeeb99576
Mick Thompson <dthompson@gmail.com>
2023-08-14 17:08:32 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="rio-vectortiles",
-    version="0.1.2",
+    version="0.1.3",
     description="Make vectortiles from rasters",
     long_description=readme,
     keywords="mapping, web mercator, tiles",
